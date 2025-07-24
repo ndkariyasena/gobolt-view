abd4b958cf66d7930b50dcd18dbb4e674dda438c
Dananjaya Ariyasena <74527180+ndkariyasena@users.noreply.github.com>
2025-07-24 23:33:36 +0200

    crud changes added (#12)
    * crud changes added
    
    * readme updated

--- web/src/routes/bucket/[name]/+page.svelte
+++ web/src/routes/bucket/[name]/+page.svelte
@@ -2,16 +2,21 @@
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 	import Search from '@lucide/svelte/icons/search';
-	import { Modal } from '@skeletonlabs/skeleton-svelte';
-	import { getKeys } from '$lib/api';
+	import { Modal, Toaster, createToaster } from '@skeletonlabs/skeleton-svelte';
+	import { getKeys, addValue, updateValue, deleteKey } from '$lib/api';
 	import type { KeyValues, KeyValuesResponse } from '$lib/types';
 	import { formatJson, isJson } from '$lib/utils';
 
+	const toaster = createToaster({
+		placement: 'top-end'
+	});
+
 	let bucketName = '';
-	let key = '';
-	let value = '';
 	let bucketKeyValues: KeyValues = [];
 	let openState = false;
+	let editState = false;
+	let actionType = '';
+	let selectedKey = '';
 	let modalContent = '';
 	let modalHeader = 'Key-Value Details';
 	let search = '';
@@ -22,16 +27,168 @@
 
 	$: bucketName = page.params.name;
 
-	function modalViewHandler(keyName?: string) {
+  const reloadPage = () => {
+    setTimeout(() => {
+      window.location.reload(); // Reload to reflect changes
+    }, 1000); // Ensure the UI reflects the changes. Time = 1000ms
+  };
+
+	const getValueForKey = (keyName: string) => {
+		const item = bucketKeyValues.find((item) => item.key === keyName);
+		return item ? item.value : '';
+	};
+
+	function modalViewHandler(keyName?: string, action?: string) {
+		actionType = action || 'view';
 		if (keyName) {
 			modalHeader = `Details for Key: ${keyName}`;
-			modalContent = bucketKeyValues.find((item) => item.key === keyName)?.value || '';
+			modalContent = getValueForKey(keyName);
 		} else {
 			modalHeader = 'Key-Value Details';
 			modalContent = '';
 		}
 		openState = !openState;
 	}
+
+	const modalEditHandler = (keyName?: string, action?: string) => {
+		actionType = action || 'edit';
+		if (keyName) {
+			selectedKey = keyName;
+			modalHeader = keyName;
+			modalContent = getValueForKey(keyName);
+
+			modalContent = isJson(modalContent) ? formatJson(modalContent) : modalContent;
+		} else {
+			selectedKey = '';
+			modalHeader = '';
+			modalContent = '';
+		}
+		editState = !editState;
+	};
+
+	const updateCurrentList = () => {
+		const index = bucketKeyValues.findIndex((item) => item.key === selectedKey);
+		if (index !== -1) {
+			bucketKeyValues[index].value = modalContent;
+		} else {
+			bucketKeyValues.push({ key: selectedKey, value: modalContent });
+		}
+		toaster.success({
+			title: 'Edits saved successfully!',
+			closable: true
+		});
+	};
+
+	const handleNewEntry = async () => {
+		if (modalHeader.trim() === '') {
+			toaster.error({
+				title: 'Key cannot be empty.',
+				closable: true
+			});
+			return;
+		} else {
+      const index = bucketKeyValues.findIndex((item) => item.key === modalHeader);
+
+      if (index !== -1) {
+        toaster.error({
+          title: `Key "${modalHeader}" already exists. Please choose a different key.`,
+          closable: true
+        });
+        return;
+      }
+
+			await addValue(bucketName, modalHeader, modalContent)
+				.then((res) => {
+					if (res.status === 200) {
+						bucketKeyValues.push({ key: modalHeader, value: modalContent });
+						toaster.success({
+							title: `Key "${modalHeader}" added successfully!`,
+							closable: true
+						});
+					} else {
+						toaster.error({
+							title: `Failed to add key "${modalHeader}". Please try again.`,
+							closable: true
+						});
+					}
+				})
+				.catch((error) => {
+					console.error('Error adding new entry:', error);
+					toaster.error({
+						title: 'Failed to add new entry. Please try again.',
+						closable: true
+					});
+				});
+		}
+	};
+
+	const updateItem = async () => {
+		const value = getValueForKey(selectedKey);
+
+		if ((selectedKey !== modalHeader || modalContent !== value) && modalHeader.trim() !== '') {
+			await updateValue(bucketName, selectedKey, modalContent, selectedKey)
+				.then(updateCurrentList)
+				.catch((error) => {
+					console.error('Error updating item:', error);
+					toaster.error({
+						title: 'Failed to update item. Please try again.',
+						closable: true
+					});
+				});
+		} else {
+			if (modalHeader.trim() === '') {
+				toaster.error({
+					title: 'Key cannot be empty.',
+					closable: true
+				});
+			} else {
+				toaster.info({
+					title: 'No changes detected.',
+					closable: true
+				});
+			}
+		}
+	};
+
+	const saveEdits = async () => {
+		if (actionType === 'add') {
+			await handleNewEntry();
+		} else if (actionType === 'edit') {
+			await updateItem();
+		}
+		editState = false;
+    
+    reloadPage(); // Reload to reflect changes
+	};
+
+	const deleteItem = async (keyName: string) => {
+		if (confirm(`Are you sure you want to delete the key "${keyName}"?`)) {
+			await deleteKey(bucketName, keyName)
+				.then((res) => {
+					if (res.status === 200) {
+						bucketKeyValues = bucketKeyValues.filter((item) => item.key !== keyName);
+						toaster.success({
+							title: `Key "${keyName}" deleted successfully!`,
+							closable: true
+						});
+    
+            reloadPage(); // Reload to reflect changes
+					} else {
+						toaster.error({
+							title: `Failed to delete key "${keyName}". Please try again.`,
+							closable: true
+						});
+					}
+				})
+				.catch((error) => {
+					console.error('Error deleting key:', error);
+					toaster.error({
+						title: 'Failed to delete key. Please try again.',
+						closable: true
+					});
+				});
+		}
+	};
 
 	onMount(async () => {
 		const res: KeyValuesResponse = await getKeys(bucketName);
@@ -40,29 +197,9 @@
 			bucketKeyValues = res.keyValues;
 		}
 	});
-
-	/* async function handleAdd() {
-    const res = await fetch(`/api/bucket/${bucketName}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ key, value }),
-    });
-    if (res.ok) {
-      items.push({ key, value });
-      key = '';
-      value = '';
-    }
-  }
-
-  async function deleteItem(k: string) {
-    const res = await fetch(`/api/bucket/${bucketName}/${k}`, {
-      method: 'DELETE',
-    });
-    if (res.ok) {
-      items = items.filter((i) => i.key !== k);
-    }
-  } */
 </script>
+
+<Toaster {toaster}></Toaster>
 
 <section class="container mx-auto max-w-7xl space-y-6 p-6">
 	<h1 class="h1 text-center">📂 Bucket: <span class="text-primary">{bucketName}</span></h1>
@@ -80,18 +217,16 @@
 	</div>
 
 	<!-- Add New Entry Form -->
-	<!-- <div class="card card-body space-y-4">
+	<div class="card card-body space-y-4">
 		<h2 class="h3">➕ Add Entry</h2>
 
-		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-			<input class="form-input" type="text" placeholder="Key" bind:value={key} />
-			<input class="form-input" type="text" placeholder="Value" bind:value />
+		<div class="flex justify-start">
+			<button
+				class="btn preset-filled-success-500"
+				onclick={() => modalEditHandler(undefined, 'add')}>Add Entry</button
+			>
 		</div>
-
-		<div class="flex justify-end">
-      <button class="btn btn-filled-primary" on:click={handleAdd}>Add Entry</button>
-    </div>
-	</div> -->
+	</div>
 
 	<!-- Key-Value List -->
 	<div class="card card-body space-y-4">
@@ -122,24 +257,38 @@
 							<td
 								class="break-inside-avoid-column border border-gray-300 bg-slate-700 p-2 dark:border-gray-700"
 							>
-								{#if isJson(item.value)}
-									<pre class="h-50 overflow-y-auto whitespace-pre-wrap">{formatJson(
-											item.value
-										)}</pre>
-								{:else}
-									<p
-										class="max-w-250 max-h-80 overflow-y-auto whitespace-pre-wrap break-words text-justify"
-									>
-										{item.value}
-									</p>
-								{/if}
+								<p
+									class="max-w-200 max-h-80 overflow-y-auto whitespace-pre-wrap break-words text-justify"
+								>
+									{item.value}
+								</p>
 							</td>
 							<td class="border border-gray-300 bg-slate-700 p-2 dark:border-gray-700">
-								<button
-									type="button"
-									class="btn btn-sm preset-filled"
-									onclick={() => modalViewHandler(item.key)}>View</button
-								>
+								<div class="mb-5 flex flex-row items-center justify-center gap-2">
+									<button
+										type="button"
+										class="btn btn-sm preset-filled mr-2"
+										onclick={() => modalViewHandler(item.key, 'view')}
+									>
+										View
+									</button>
+									<button
+										type="button"
+										class="btn btn-sm preset-filled-warning-500"
+										onclick={() => modalEditHandler(item.key, 'edit')}
+									>
+										Edit
+									</button>
+								</div>
+								<div class="mb-4 flex flex-row items-center justify-center gap-2">
+									<button
+										type="button"
+										class="btn btn-sm preset-filled-error-500"
+										onclick={() => deleteItem(item.key)}
+									>
+										Delete
+									</button>
+								</div>
 							</td>
 						</tr>
 					{/each}
@@ -171,7 +320,52 @@
 				{/if}
 			</article>
 			<footer class="flex shrink-0 justify-end gap-4">
-				<button type="button" class="btn preset-filled" onclick={() => modalViewHandler(undefined)}>
+				<button
+					type="button"
+					class="btn preset-filled"
+					onclick={() => modalViewHandler(undefined, '')}
+				>
+					Close
+				</button>
+			</footer>
+		{/snippet}
+	</Modal>
+
+	<Modal
+		open={editState}
+		onOpenChange={(e) => (editState = e.open)}
+		triggerBase="btn preset-tonal"
+		contentBase="card bg-surface-100-900 p-5 space-y-4 shadow-xl w-dvh h-75 max-w-full max-h-75vh overflow-y-auto flex flex-col h-full"
+		backdropClasses="backdrop-blur-sm"
+	>
+		{#snippet content()}
+			<header class="flex justify-between">
+				<h2 class="h2">Edit {selectedKey}</h2>
+			</header>
+			<article class="flex-1 overflow-y-auto">
+				<input
+					type="text"
+					bind:value={modalHeader}
+					class="form-input mb-5 w-full bg-slate-700"
+					placeholder="Edit key here..."
+					tabindex="0"
+				/>
+				<textarea
+					rows="20"
+					bind:value={modalContent}
+					class="form-textarea mt-2 w-full bg-slate-700"
+					placeholder="Edit value here..."
+				></textarea>
+			</article>
+			<footer class="flex shrink-0 justify-end gap-4">
+				<button type="button" class="btn preset-filled-tertiary-500" onclick={saveEdits}>
+					Save
+				</button>
+				<button
+					type="button"
+					class="btn preset-filled"
+					onclick={() => modalEditHandler(undefined, '')}
+				>
 					Close
 				</button>
 			</footer>
